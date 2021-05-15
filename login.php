59e7692c154facb45b89f765f006f703adc0fdcc
blerandrudhani <58183465+blerandrudhani@users.noreply.github.com>
2021-05-15 23:41:31 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php 
+
+include 'dbconnect.php';
+
+
+
+if (isset($_POST['loginbtn'])) {
+	$username=stripslashes($_POST["username"]);
+
+    $password=stripslashes(md5($_POST["password"]));
+
+ $stmt = $conn->prepare("SELECT * FROM users WHERE Username=?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    // verifying the password
+    if ($username==$user['Username'] && $password==$user['Password'] )
+    {
+        // starts the session created if login info is correct
+        session_start();
+        $_SESSION['username'] = $user['Username'];
+       
+        header("Location: sdsdsd.php");
+        
+        exit;
+    } else {
+
+        $error = "Login and password don't match";
+       echo "ERROR: ".$error;
+    }
+	
+}
+
+
+	 ?>
+	
+   
+
+    <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />    
+
+    <link rel="stylesheet" type="text/css" href="style/fonts.css"/> 
+    <link rel="stylesheet" type="text/css" href="style/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="style/main.css"/>
+
+	<title>Log In</title>
+	<style type="text/css">
+		#logindiv{
+			width: 500px;
+			height: 500px;
+			text-align: center;
+			margin-left: 500px;
+			margin-top: 100px; 
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			border-radius: 20px;
+
+		}
+
+		.logininput{
+			width: 300px;
+			height: 50px;
+			margin-top: 50px;
+			border-radius: 20px;
+			outline: none;
+			border: 0.5px grey solid;
+			background-color: white;
+		}
+		button{
+			margin-top: 50px;	
+			height: 50px;
+			width: 200px;
+			color: white;
+			background-color: black;
+			border-radius: 10px;
+			outline: none;
+
+		}
+
+		#logintitle{
+			font-size: 35px;
+			font-weight: bold;
+		}
+
+	</style>
+</head>
+<body>
+
+
+
+
+	
+
+
+
+	<form action="login.php" method="post">
+
+<div id="logindiv">
+	
+		<h1 id="logintitle">Log in</h1>
+		
+<input class="logininput" type="text" name="username" placeholder="Username ....." required style="background-color: white;"><br/>
+<input class="logininput" type="password" name="password" placeholder="Password" required style="background-color: white;"><br/>
+
+<button name="loginbtn">LogIn</button> <br/><br/>
+<p>Dont have an account <a href="signup.php">Sign Up </a> here</p>
+</div>
+
+</form>
+
+
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <?php
+
+include 'dbconnect.php';
+
+$domains=["hotmail.com","gmail.com","outlook.com"];
+
+
+if(isset($_POST["signupbtn"])){
+
+
+	$username=stripslashes($_POST["username"]);
+	$email=stripslashes($_POST['email']);
+	$password=stripslashes(md5($_POST["password"]));
+	$cpassword=stripslashes(md5($_POST["comfpassword"]));
+
+
+$regex ="/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/" ;
+$res=preg_match($regex, $_POST['password']);
+if(!$res){
+
+header("Location: signup.php?$res");
+
+
+exit;}else{echo "Rezultati : ".$res; exit;}
+
+	
+if($password==$cpassword){
+
+$email_chunks=explode("@", $email);
+if ($email_chunks[1]==$domains[0] || $email_chunks[0]==$domains[1] || $email_chunks[0]==$domains[2]) 
+{
+	
+ 
+
+	}
+else{ echo "<script> alert('hotmail , gmail , outlook  are allowed only ')</script>"; }
+
+
+	$query="INSERT INTO users(Username,Email,Password) VALUES ('$username','$email','$password')";
+	$conn->exec($query);
+	echo "<script>alert(New record created succesfully )</script>";
+
+}
+else{
+
+	echo "<script>alert('Passwords dont match !!!' )</script>";
+}
+}
+
+	 ?>
+	<title>Sign Up</title>
+	<style type="text/css">
+		div{
+			width: 500px;
+			height: 600px;
+			text-align: center;
+			margin-left: 500px;
+			margin-top: 100px; 
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			border-radius: 20px;
+
+
+		}
+
+		input{
+			width: 300px;
+			height: 50px;
+			margin-top: 50px;
+			border-radius: 20px;
+			outline: none;
+			border: 0.5px grey solid;
+		}
+		button{
+			margin-top: 35px;	
+			height: 50px;
+			width: 200px;
+			color: white;
+			background-color: black;
+			border-radius: 10px;
+			outline: none;
+
+		}
+	legend{
+		font-size: 30px;
+	}
+	h1{
+		margin-top: 20px;
+	}
+
+	</style>
+</head>
+<body>
+
+
+
+
+	
+
+
+
+	<form action="signup.php" method="post">
+
+
+	<div>
+		<h1>Sign Up</h1>
+		
+<input type="text" name="username" placeholder="Username ....." required><br/>
+<input type="email" name="email" placeholder="Email" required><br/>
+<input type="password" name="password" placeholder="Password" id="pas" required title="Password must contain: 8 characters , an uppercase , a digit"><br/>
+<input type="password" name="comfpassword" placeholder="Comfirm Password" required><br/>
+
+
+
+<button name="signupbtn">Sign Up</button>
+<p>Have an account <a href="login.php">Log in </a> here</p>
+</div>
+</form>
+<script type="text/javascript">
+
+
+	
+</script>
+
+
+</body>
+</html>
