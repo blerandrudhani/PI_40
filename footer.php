61ee92cc0a232c9b52c1106662a4abb69ef07900
blerandrudhani <58183465+blerandrudhani@users.noreply.github.com>
2021-05-18 22:14:57 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,60 @@
+ 
+
+
+<footer>
+        <div class="gradient-bg1 gradient-padding-bg">
+            <div class="content-three-column-container pt30 pb30">
+                <div class="text-center">
+                    <div class="special-titles mb30 mt30 text-center">
+                        <h3>Explore</h3>
+                    </div>
+
+                    <table class="navF">
+                        <tr>
+                            <td align="right"><a href="about.html">About</a></td>
+                            <td align="left"><a href="candidates.html">Candidates</a></td>
+                        </tr>
+                        <tr>
+                            <td align="right"><a href="services.html">Services</a></td>
+                            <td align="left"><a href="contact.html">Contacts</a></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="text-center">
+                    <img src="images/logo.png" class="mb30" alt=""/>
+                    <p class="mb30 text-italic">
+                        Human resources is the set of the people who make up the workforce of an organization, business sector, industry, or economy. A narrower concept is human capital, the knowledge which the individuals embody.
+                    </p>
+                    <div class="contactF">
+                        <address>
+                            <p>
+                                <i class="fa fa-map-marker highlight"></i> 10000 Prishtina, Kosova
+                            </p>
+                            <p>
+                                <a href="tel:+101 123 456 789"
+                                ><i class="fa fa-phone highlight"></i> +101 123 456 789</a
+                                >
+                            </p>
+                            <p>
+                                <a href="mailto:example@example.com"
+                                ><i class="fa fa-envelope highlight"></i>
+                                    example@example.com</a
+                                >
+                            </p>
+                        </address>
+                    </div>
+                </div>
+                <video class="mt30" width="100%" controls>
+                    <source src="repository/hr.mp4" type="video/mp4"/>
+                    <source src="movie.ogg" type="video/ogg"/>
+                </video>
+            </div>
+        </div>
+
+        <!-- <div id="animateMsgDiv"></div>
+        <div id="animateDiv" class="redDiv"></div> -->
+
+        <div class="copyright">
+            <p>© Copyright 2021 All Rights Reserved</p>
+        </div>
+    </footer>
