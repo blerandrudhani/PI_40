5267fd933a2e7027ad196147fa7e4ebcdd1d7c91
blerandrudhani <58183465+blerandrudhani@users.noreply.github.com>
2021-05-21 22:28:25 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -11,8 +11,6 @@ USE `projekti`;
 
 CREATE TABLE `projekti`.`users` 
 ( `Uid` INT NOT NULL AUTO_INCREMENT ,
-`Name` VARCHAR(255) NOT NULL ,
-`Surname` VARCHAR(255) NOT NULL ,
 `Username` VARCHAR(255) NOT NULL ,
 `Email` VARCHAR(255) NOT NULL ,
 `Password` VARCHAR(255) NOT NULL,
