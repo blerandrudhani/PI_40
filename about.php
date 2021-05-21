eeacd032d49ea22dd4c3e9eeca7d65386f56a70d
Ermirvishi <75865381+Ermirvishi@users.noreply.github.com>
2021-05-21 21:56:18 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Hr - About</title>
+
+    <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="stylesheet" type="text/css" href="style/fonts.css"/>
+    <link rel="stylesheet" type="text/css" href="style/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="style/main.css"/>
+</head>
+<body>
+<div class="wrapper-page">
+
+    <?php include 'header.php'; ?>
+
+    <section class="banner text-center">
+        <div class="banner-description">
+            <h2>About</h2>
+            <p>HOME &nbsp; / &nbsp; ABOUT</p>
+        </div>
+    </section>
+
+    <section class="content-two-column-container align-items-center pt30">
+        <div class="content-left">
+            <div class="special-heading">
+                <h2 class="extra-heading">HR</h2>
+                <h3>Welcome to</h3>
+                <h2>Agency!</h2>
+            </div>
+            <div class="text-block">
+                <p>
+                    We believe in the value that our functions add to a business. We
+                    feel that this specialist part of HR is often unrecognised for its
+                    contribution to the profitability and success of a business.
+                </p>
+                <ul class="icons-list">
+                    <li>
+                        <i class="fa fa-rocket"></i>
+                        <h4><span>638</span> Companies We Helped</h4>
+                    </li>
+                    <li>
+                        <i class="fa fa-briefcase"></i>
+                        <h4><span>12</span> Corporate Programs</h4>
+                    </li>
+                    <li>
+                        <i class="fa fa-graduation-cap"></i>
+                        <h4><span>28</span> Trainings Courses</h4>
+                    </li>
+                    <li>
+                        <i class="fa fa-user"></i>
+                        <h4><span>125</span> Strategic Partners</h4>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="content-right">
+            <img src="repository/person31.png" alt=""/>
+        </div>
+    </section>
+
+   <?php include 'footer.php'; ?>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
